69fe319eb66a2abda18d2c4bdd61c6a730b80fc3
andygarciaprog1_203 <agarciacaro8@gmail.com>
2025-03-04 14:36:58 -0500

    updated

---
+++ Taller_entrega_06-03/Punto1.py
@@ -0,0 +1,10 @@
+#Determinar los divisores de un número introducido por teclado
+
+n1 = int(input("Digite un numero "))
+
+
+for i in range (1,n1+1):
+   if n1%i==0:
+      print(f"{i} es divisor")
+   else:
+      print(i)
