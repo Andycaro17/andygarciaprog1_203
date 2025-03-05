7e12b0b1db095f044a13cb005fe290eebc687af9
andygarciaprog1_203 <agarciacaro8@gmail.com>
2025-03-04 21:15:37 -0500

    uploaded

---
+++ Taller_entrega_06-03/Punto4.py
@@ -0,0 +1,12 @@
+#Determinar cuales y cuantos números perfectos hay entre 1 y 1000? 
+
+
+for i in range(1,1001):
+    j = 2 
+    suma = 0 
+    while j <= i:
+        if i%j==0:
+         suma += i // j 
+        j += 1 
+    if suma == j:
+        print (i)
